2234242a8480e245b54d87643d3d14e655d6af02
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2015-05-11 17:58:21 +0900

    Fix pointsandlines

--- pointsandlines/cc-correct/main.cc
+++ pointsandlines/cc-correct/main.cc
@@ -3,6 +3,8 @@
 #include <set>
 #include <cassert>
 #include <algorithm>
+// #define dump(x) { cerr << " [L" << __LINE__ << "] " << #x << " = " << (x) << endl; }
+#define dump(x) {}
 using namespace std;
 
 typedef pair<int, int> Point;
@@ -43,17 +45,26 @@ int main()
           // pa should be left
           swap(pa, pb);
         }
+        // fprintf(stderr, "\n=== (%d, %d) and (%d, %d) ===\n", pa.second, pa.first, pb.second, pb.first);
 
-        int dy = abs(pa.first - pb.second);
+        int dy = abs(pa.first - pb.first);
         int dx = abs(pa.second - pb.second);
         int sy = (pb.first - pa.first) < 0 ? -1 : 1;
         int sx = (pb.second - pa.second) < 0 ? -1 : 1;
+        dump(dy); dump(dx); dump(sy); dump(sx);
         assert(sx == 1);
         int cd = gcd(dy, dx);
         if (cd == 0) {
           // either dx or dy is zero, so increment non-zero delta by one
           cd = 1;
         }
+        dump(cd);
+
+        int ay = dy / cd;
+        int ax = dx / cd;
+        if (ay == 0) { ax = 1; }
+        if (ax == 0) { ay = 1; }
+        dump(ax); dump(ay);
 
         Point pt = pa;
         int y = pt.first;
@@ -67,11 +78,13 @@ int main()
 
         int cnt = 0;
         while (-100 <= y && y <= 100 && -100 <= x && x <= 100) {
+          // fprintf(stderr, "trying... (%d, %d)\n", x, y);
           if (points.find(Point(y, x)) != points.end()) {
+            // fprintf(stderr, "  point found: (%d, %d)\n", x, y);
             cnt++;
           }
-          x += sx * dx * cd;
-          y += sy * dy * cd;
+          x += sx * ax;
+          y += sy * ay;
         }
         ans = max(ans, cnt);
       }
